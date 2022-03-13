611e0ee5925c510a0bd706229047b8fd7d05ca89
bcExpt1123 <activitiesapphelpdesk@gmail.com>
2022-03-14 01:09:07 +0200

    deploy contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+const { artifacts } = require("hardhat");
 const hre = require("hardhat");
 
 async function main() {
@@ -19,8 +20,29 @@ async function main() {
   const avaxPrice = await AvaxPrice.deploy("0x5498BB86BC934c8D34FDA08E81D444153d0D06aD");
 
   await avaxPrice.deployed();
-
+  saveDappFiles(avaxPrice);
   console.log("avaxPrice deployed to:", avaxPrice.address);
+}
+
+function saveDappFiles(contract) {
+  const fs = require('fs');
+  const contractDir = __dirname + '/../frontend/contracts';
+  if (!fs.existsSync(contractDir)) {
+    fs.mkdirSync(contractDir);
+  }
+  const addressFileName = contractDir + '/avax-price.json';
+  fs.writeFileSync(
+    addressFileName,
+    JSON.stringify({ Contract: contract.address }, undefined, 2)
+  );
+  console.log('Stored address in ', addressFileName);
+  const ContractArtifact = artifacts.readArtifactSync('AvaxPrice');
+  const artifactFileName = contractDir + '/AvaxPrice.json';
+  fs.writeFileSync(
+    artifactFileName,
+    JSON.stringify(ContractArtifact, null, 2)
+  );
+  console.log('Stored artifact in ', artifactFileName)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
