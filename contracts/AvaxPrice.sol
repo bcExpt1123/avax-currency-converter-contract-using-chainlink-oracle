93d8707686435bcea72b15ebebbc74829ce53a29
bcExpt1123 <activitiesapphelpdesk@gmail.com>
2022-03-14 03:29:55 +0200

    update convert currency function

--- contracts/AvaxPrice.sol
+++ contracts/AvaxPrice.sol
@@ -25,7 +25,7 @@ contract AvaxPrice {
         return _price;
     }
 
-    function convertCurrency(int256 _usdAmount) public view returns (int256, int256) {
+    function convertCurrency(int256 _usdAmount) public view returns (string memory) {
         (
             /*uint80 roundID*/,
             int256 _price,
@@ -33,8 +33,15 @@ contract AvaxPrice {
             /*uint timeStamp*/,
             /*uint80 answeredInRound*/
         ) = priceFeed.latestRoundData();
-        int256 avaxAmount = _usdAmount.mul(10 ** 16).div(_price);
-        return (avaxAmount, 10 ** 8);
+        int256 avaxAmount = _usdAmount.mul(10 ** 8).div(_price);
+        int256 modAmount = _usdAmount.mul(10 ** 8).mod(_price);
+        string memory avaxAmountStr = Strings.toString(uint256(avaxAmount));
+        avaxAmountStr = string(abi.encodePacked(avaxAmountStr, '.'));
+        for(int256 i = 0; i < 8; i ++) {
+            avaxAmountStr = string(abi.encodePacked(avaxAmountStr, Strings.toString(uint256(modAmount.mul(10).div(_price)))));
+            modAmount = modAmount.mul(10).mod(_price);
+        }
+        return (avaxAmountStr);
     }
 
 }
